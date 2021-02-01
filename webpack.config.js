d1d584d0f1c4d89b7cf0ae465023c447c77bcfcd
Ricardo Guerrero <richi_gro@hotmail.com>
2021-02-01 11:15:13 -0600

    added style and css-loader to enable modular react components all css will be injected from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,28 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  //specifid the file and directory for compiled code 
+  // the source code that needs to be compiled
   entry: [`${SRC_DIR}/index.js`],
+  //this will not minify code, so that debugging is easier
   mode: 'development',
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
   },
+  //dev server used for easier development so you wont have to reload
+  // page everytime there is a change
+  // devServer: {
+  //   port: 8080,
+
+  // },
+
+  //this is what compiles my react code from the src directory
+  //and also es6 code, presets are contained within the .babelrc file
+
   module: {
     rules: [
+      //babel-loader with env and react presets
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
@@ -20,21 +34,21 @@ module.exports = {
           {
             loader: 'babel-loader',
             options: {
+              //this will avoid re-compilation if nothing changes
               cacheDirectory: true,
             },
           },
         ],
       },
+      //style and css loaders
+      //css and style loaders to be able to write modular
+      //react components along with theit own style sheets
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/i,
-  //       use: ['style-loader', 'css-loader'],
-  //     },
-  //   ],
-  // },
   resolve: {
     extensions: ['*', '.js', '.jsx', '.css'],
   },
